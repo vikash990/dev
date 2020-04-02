51a11bb13b1e83f5706b83632e56e6e4ca0ecd19
vikash990 <vikashadubey>
2020-04-03 00:47:23 +0530

    Updates

---
+++ precache-manifest.42bb5727c8526e02f2cabdb487e6ca71.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5915d26da02835fcef4e0985ab4715f",
+    "url": "/github.io/newTech/index.html"
+  },
+  {
+    "revision": "3ad19732de07af0ee0b3",
+    "url": "/github.io/newTech/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "0439d63655abd0df94b1",
+    "url": "/github.io/newTech/static/css/main.36c7c39e.chunk.css"
+  },
+  {
+    "revision": "3ad19732de07af0ee0b3",
+    "url": "/github.io/newTech/static/js/2.8e226794.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/github.io/newTech/static/js/2.8e226794.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0439d63655abd0df94b1",
+    "url": "/github.io/newTech/static/js/main.38054a6d.chunk.js"
+  },
+  {
+    "revision": "3513a4f3e9b0dac9bd97",
+    "url": "/github.io/newTech/static/js/runtime-main.206762ec.js"
+  }
+]);
